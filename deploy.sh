78c9bd2c09ed1480ce69e9698b9d9cf36d71f70c
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-10-01 11:40:34 +0530

    modifed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-
-git clone https://github.com/palpriyanshu/Library-react.git 2> /dev/null
-cd Library-react
-echo "installing"
-npm install 2> /dev/null
-npm run test
-
-cd ..
 git clone https://github.com/palpriyanshu/library-backend.git 2> /dev/null
 cd library-backend
 echo "installing"
 npm install 2> /dev/null
 npm run test
 
+rm -rf ./public
 mkdir ./public
 
-cd ../Library-react
+cd ..
 echo "building"
 npm run build 2> /dev/null
 
